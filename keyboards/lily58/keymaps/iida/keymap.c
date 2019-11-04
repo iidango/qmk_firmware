f65a9a295f9af1c086cebe22ffa1e0505877274d
iida <iida@ucl.nuee.nagoya-u.ac.jp>
2019-11-04 14:13:22 +0900

    Put escape key to raise-:

--- keyboards/lily58/keymaps/iida/keymap.c
+++ keyboards/lily58/keymaps/iida/keymap.c
@@ -85,7 +85,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | Tab  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |LCTRL |   +  |   -  |   *  |   /  |   =  |-------.    ,-------| Left | Down |  Up  |Right |      |      |
+ * |LCTRL |   +  |   -  |   *  |   /  |   =  |-------.    ,-------| Left | Down |  Up  |Right | ESC  |      |
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
  * |LShift|   |  |   _  |   ~  |   \  |      |-------|    |-------|   +  |   -  |   =  |   [  |   ]  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
@@ -97,7 +97,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT( \
   KC_SLEP, _______, _______, _______, _______, _______,                     _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, \
   KC_TAB,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, \
-  KC_LCTRL, KC_PLUS, KC_MINS, KC_ASTR, KC_SLSH, KC_EQL,                       KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, XXXXXXX, \
+  KC_LCTRL, KC_PLUS, KC_MINS, KC_ASTR, KC_SLSH, KC_EQL,                       KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_ESC, XXXXXXX, \
   KC_LSFT, KC_PIPE, KC_UNDS, KC_TILD, KC_BSLS, _______,   _______, _______,  KC_PLUS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_RSFT, \
                              _______, _______, _______,  _______, _______,  _______, _______, _______ \
 ),
